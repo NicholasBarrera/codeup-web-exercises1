d168dafc826df0baf895b137988b973fb04877e9
Nicholas Barrera <nickbarerra722@gmail.com>
2022-09-06 18:05:16 -0500

    exercises

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,3 +1,12 @@
 $(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+var text = $('#second-item').text()
+console.log(text)
+
+$('.codeup').css('border', "1px solid red");
+
+alert($('h1').text());
+
+
