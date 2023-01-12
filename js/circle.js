df232a2ca92bc26d05ecd0e3dd0fed40183086be
Nicholas Barrera <nickbarerra722@gmail.com>
2023-01-12 15:49:54 -0600

    practice function #1

--- js/circle.js
+++ js/circle.js
@@ -1,40 +1,94 @@
-(function() {
-    "use strict";
+SHAZAM CORE
+
+import {buildCreateApi, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { build } from 'vite';
 
 
-    // create a circle object
-    var circle = {
-        radius: 3,
-        getArea: function () {
-            // TODO: complete this method
-            // hint: area = pi * radius^
-            let area = Math.PI * Math.pow(this.radius, 2);
-            return area; // TODO: return the proper value
+export const shazamCoreApi = createApi ({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Host', '2eb6eea384msh367af004d5e39e8p111377jsnd388daacae12');
 
+            return headers;
         },
 
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: '/charts/world'}),
+    })
+});
 
-        logInfo: function (doRounding) {
-            // TODO: complete this method.
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
 
-            // Round the result to the nearest integer
-            let roundArea = Math.round(this.getArea());
-            console.log(`Area of a circle with radius: ${this.radius}, is: ${roundArea}`);
-        }
-    };
 
-    // log info about the circle
-    console.log("Raw circle information");
-    circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
+STORE.JS
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
-    console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
-    circle.radius = 5;
-    // log info about the circle
-    console.log("Raw circle information");
-    circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
-})();
+import playerReducer from './features/playerSlice';
+import { shazamCoreApi  } from './services/shazamCore';
+
+export const store = configureStore({
+    reducer: {
+        [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
+        player: playerReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shazamCoreApi.middleware),
+});
+
+
+
+DISCOVER.JS
+import { Error, Loader, SongCard } from '../components'
+import { genres } from '../assets/constants';
+
+
+const Discover = () => {
+    const genreTitle = 'Pop';
+
+    return (
+        <div className='flex flex-col'>
+            <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
+                <h2 className='font-bold text-3xl text-white'>
+                    Discover { genreTitle }
+                </h2>
+                <select
+                    onChange={() => {}}
+                    value=""
+                    className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
+
+                >
+                    {genres.map((genre) => <option key={genre.value}
+                                                   value={genre.value}>{genre.title}</option>)}
+                </select>
+            </div>
+
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {[1,2,3,4,5,6,7,8,9,10].map((song, i) => (
+                    <SongCard
+                        key={song.key}
+                        song={song}
+                        i={i}
+                    />
+                ))};
+            </div>
+
+        </div>
+    );
+};
+
+export default Discover;
+
+//Practice Javascript exercises
+
+//Write a JavaScript function that reverse a number
+
+function reverse_a_number(n)
+{
+    n = n + "";
+    return n.split("").reverse().join("");
+}
+console.log(Number(reverse_a_number(32243)));
