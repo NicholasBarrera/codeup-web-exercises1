ebc5be478ca9ff42f7720706896e480f50345113
Nicholas Barrera <nickbarerra722@gmail.com>
2023-01-14 15:57:40 -0600

    update

--- js/circle.js
+++ js/circle.js
@@ -198,3 +198,24 @@ function uppercase(str)
     return newarray1.join(' ');
 }
 console.log(uppercase("the quick brown fox"));
+
+
+//Write a JavaScript function that accepts a string as a parameter and find the longest word within the string. Go to the editor
+// Example string : 'Web Development Tutorial'
+// Expected Output : 'Development'
+
+function find_longest_word(str)
+{
+    var array1 = str.match(/\w[a-z]{0,}/gi);
+    var result = array1[0];
+
+    for(var x = 1 ; x < array1.length ; x++)
+    {
+        if(result.length < array1[x].length)
+        {
+            result = array1[x];
+        }
+    }
+    return result;
+}
+console.log(find_longest_word('Web Development Tutorial'));
