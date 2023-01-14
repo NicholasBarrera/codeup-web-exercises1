43c18ae8ed074f42fb900f9628ceeee997c60d7e
Nicholas Barrera <nickbarerra722@gmail.com>
2023-01-14 15:55:48 -0600

    update

--- js/circle.js
+++ js/circle.js
@@ -168,3 +168,14 @@ function substrings(str1)
 }
 
 substrings("dog");
+
+//Write a JavaScript function that returns a passed string with letters in alphabetical order. Go to the editor
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
+
+function alphabet_order(str)
+{
+    return str.split('').sort().join('');
+}
+console.log(alphabet_order("webmaster"));
